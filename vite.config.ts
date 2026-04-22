2ef78e4b43ef0ef74c2412f9ab46f1ac6fa0ccd9
Maximiliano David Ducoli <maxiducoli@gmail.com>
2026-04-22 16:05:36 -0300

    Primer subida
    HTML - CSS completo

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        //d:aplicaion/dist/
+        index: resolve(__dirname, "index.html"),
+        storeHome: resolve(__dirname, "src/pages/store/home/home.html"),
+        storeCart: resolve(__dirname, "src/pages/store/cart/cart.html"),
+      },
+    },
+  },
+  base: "./",
+  css: {
+     preprocessorOptions: {
+        scss: {
+          silenceDeprecations: [
+            'import',
+            'mixed-decls',
+            'color-functions',
+            'global-builtin',
+          ],
+        },
+     }
+    },
+});
